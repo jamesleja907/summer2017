284c0078963cc99e88f1105f3f3da938d2734835
Christopher Stalzer <christopher.stalzer@mail.utoronto.ca>
2017-04-27 14:29:45 -0400

    actual changes

--- QuizApp/src/quiz_app/Game.java
+++ QuizApp/src/quiz_app/Game.java
@@ -1,5 +1,41 @@
 package quiz_app;
-
-public class Game {
+/**
+ * 
+ * An abstract Game class
+ *
+ */
+public abstract class Game {
+	
+	/** Check if player is ready */
+	protected boolean ready;
+	/** The time allotted for the game */
+	protected int time;
+	/** The score for the game */
+	protected int score;
+	
+	
+	public Game(){
+		
+	}
+	
+	//	public Question askQuestion(){
+	//	
+	//}
+	/**
+	 * Compares the selected/inputed answer to the correct answer. 
+	 * 
+	 */
+	public boolean judgeAnswer(){
+		return true;
+	}
+	/**
+	 * reward points based on a correct answer
+	 * @return score
+	 */
+	public int updateScore(){
+		if (judgeAnswer()){
+		score += 10;
+		} return score;
+	}
 
 }
