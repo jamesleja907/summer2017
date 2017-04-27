fb2a994797a4b7b567c858e4cf8030638acb2521
James Leja <james.leja@mail.utoronto.ca>
2017-04-27 12:09:33 -0400

    commit with package and question

---
+++ QuizApp/src/quiz_app/Question.java
@@ -0,0 +1,15 @@
+package quiz_app;
+
+import java.io.Serializable;
+
+public class Question implements Serializable {
+	
+	/**
+	 * A question object.
+	 */
+	private static final long serialVersionUID = 1482532405230307666L;
+	
+	/** The unique id of the Question. */
+	private String id;
+
+}
