45c875651bf63ae215a6dba6a6a5b25b277ffde5
James Leja <james.leja@mail.utoronto.ca>
2017-05-03 14:08:10 -0400

    Skeleton for Account

--- QuizApp/src/quiz_app/Account.java
+++ QuizApp/src/quiz_app/Account.java
@@ -1,5 +1,63 @@
 package quiz_app;
 
+/**
+ * The superclass for Players and Admins.
+ */
 public class Account {
-
+	
+	/** The login to the account.*/
+	private String login;
+	/** The password of the account.*/
+	private String password;
+	
+	/**
+	 * 
+	 * @param login
+	 * 		The login to the account.
+	 * @param password
+	 * 		The password of the account.
+	 */
+	public Account(String login, String password){
+		this.login = login;
+		this.password = password;
+	}
+	
+	/**
+	 * 
+	 * @return the login of the account.
+	 */
+	public String getLogin() {
+		return login;
+	}
+	
+	/**
+	 * 
+	 * @param login
+	 * 		The new login of the account.
+	 */
+	public void setLogin(String login) {
+		// Check all other login to make sure the String new_login is unique.
+		this.login = login;
+	}
+	
+	/**
+	 * 
+	 * @return the password of the account.
+	 */
+	public String getPassword() {
+		return password;
+	}
+	
+	/**
+	 * 
+	 * @param password
+	 * 		The new password of the account.
+	 */
+	public void setPassword(String old_password, String new_password) {
+		// A user can only change the password if he/she can correctly provide
+		// the old password.
+		if (this.password == old_password) {
+			this.password = new_password;
+		}
+	}
 }
