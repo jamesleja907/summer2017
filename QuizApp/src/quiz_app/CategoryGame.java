9d237336bebe4b9caf9239a2cc32bc400b0a17ec
Christopher Stalzer <christopher.stalzer@mail.utoronto.ca>
2017-05-03 16:04:13 -0400

    added to cat game

--- QuizApp/src/quiz_app/CategoryGame.java
+++ QuizApp/src/quiz_app/CategoryGame.java
@@ -1,6 +1,10 @@
 package quiz_app;
 
 import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 /**
  * 
  * A Game based on category
@@ -8,25 +12,65 @@ import java.util.ArrayList;
  */
 public class CategoryGame extends Game {
 	
+	private QuestionManager qm;
 	protected String category;
-	
+	private int difficulty;
 	private int score;	
 	private int time;
+	private Random randomGenerator;
+	private Timer timer;
+	private ArrayList<Question> questions;
+	private Scanner scanner;
 	
-	public CategoryGame(String category){
-		this.category = category;
-		
-	}	
+	
 	/**
-	 * Groups questions based on category
-	 * @return ArrayList
+	 * Create a CategoryGame based on the desired category and difficulty
+	 * @param category
+	 * @param difficulty
 	 */
-	public ArrayList getQuestions(){
-		ArrayList questions = new ArrayList(); 
+	public CategoryGame(String category, int difficulty){
+		this.category = category;
+		this.difficulty = difficulty;
+		scanner = new Scanner(System.in);
+		qm = QuestionManager.getInstance();
+		randomGenerator = new Random();
+		ArrayList<Question> questions = buildQuestions();
 		
+	}
+	/**
+	 * Build an ArrayList of Questions to be used in this Game
+	 * @return
+	 */
+	public ArrayList<Question> buildQuestions(){
+		ArrayList<Question> qs = qm.getQuestions(category);
+		for (Question q : qs){
+			if (q.getDifficulty() != this.difficulty){
+				qs.remove(q);
+			}
+		}
+		ArrayList<Question> questions = new ArrayList<Question>();
+		if (qs.size() <= 10){
+			return qs;
+		}
+		for (int i = 0; i < 10; i++){
+			int index = randomGenerator.nextInt(qs.size());
+			questions.add(qs.get(index));
+			qs.remove(index);
+		}
 		return questions;
 		
 	}
-	
+	class askQuestion extends TimerTask{
+
+		@Override
+		public void run() {
+			Question q = questions.get(0);
+			System.out.println(q.getQuestion());
+//			boolean correct = false;
+			String answer = scanner.next();
+			
+		}
+		
+	}
 
 }
