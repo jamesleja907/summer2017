e2abfdd752b6f394edecc69ace4887101fe3e81b
Runhao Liang <dam0nl12@A-DAMON-MacBook-Pro.local>
2017-05-07 23:57:19 -0400

    Still some format part

--- QuizApp/src/quiz_app/TextFieldMouseListener.java
+++ QuizApp/src/quiz_app/TextFieldMouseListener.java
@@ -1,19 +1,20 @@
 package quiz_app;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JTextField;
-	
+
 /**
  * 
- *  A Mouse Listener to set text fields to empty strings when clicked on. Follows
- * 	
-*/
+ * A Mouse Listener to set text fields to empty strings when clicked on. Follows
+ * 
+ */
 public class TextFieldMouseListener extends MouseAdapter {
 
 	/**
-	* Sets a JTextField to the empty string when MouseEvent mouseClicked takes
-	* place in a JTextField.
-	*/
+	 * Sets a JTextField to the empty string when MouseEvent mouseClicked takes
+	 * place in a JTextField.
+	 */
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		JTextField txtArea = (JTextField) e.getSource();
@@ -21,5 +22,3 @@ public class TextFieldMouseListener extends MouseAdapter {
 	}
 
 }
-
-
